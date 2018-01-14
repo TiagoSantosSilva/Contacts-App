b60204932c83494606ddf60b9a5af5b01bc93f96
Tiago Santos <tiagossilva_95@hotmail.com>
2018-01-14 14:53:33 +0000

    Initial Commit

---
+++ Contacts-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contacts-App
+//
+//  Created by Tiago Santos on 14/01/18.
+//  Copyright © 2018 Tiago Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
